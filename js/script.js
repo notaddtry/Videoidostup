598247f923e8a76f284742e839d45dbcda3e2aaa
notaddtry <comatose.9999@mail.ru>
2021-05-10 22:15:21 +0300

    added to git

--- js/script.js
+++ js/script.js
@@ -50,31 +50,11 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0 // For Safari
-  document.getElementById("top").scrollIntoView({
+  document.querySelector(".header").scrollIntoView({
     behavior: "smooth",
     block: "start",
   }) // For Chrome, Firefox, IE and Opera
 }
-//---------------------
-
-// Scrolling when clicking
-
-//const anchors = document.querySelectorAll('a[href*="#"]')
-
-//for (let anchor of anchors) {
-//  anchor.addEventListener("click", function (e) {
-//    e.preventDefault()
-
-//   const blockID = anchor.getAttribute("href").substr(1)
-
-//   document.getElementById(blockID).scrollIntoView({
-//     behavior: "smooth",
-//     block: "start",
-//   })
-// })
-//}
-//https://ru.stackoverflow.com/questions/676303/%D0%9F%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F-%D0%BF%D1%80%D0%BE%D0%BA%D1%80%D1%83%D1%82%D0%BA%D0%B0-%D0%BA-%D1%8F%D0%BA%D0%BE%D1%80%D1%8E-%D0%B1%D0%B5%D0%B7-jquery
-//https://www.youtube.com/watch?v=zs1r8yafTE8
 
 // Burger menu
 
